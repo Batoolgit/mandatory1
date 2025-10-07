35aad107314447ef3f7182ed88e228b719fdc5ba
batoolgit <batooakbar@gmail.com>
2025-10-07 17:21:52 +0200

    fix ipyhon error

--- Wave2D.py
+++ Wave2D.py
@@ -6,8 +6,6 @@ import scipy.sparse as sparse
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import matplotlib.animation as animation
-from IPython.display import HTML
-from IPython.display import display
 
 
 
